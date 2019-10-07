134d7d9cbe617659ac85c72d90337b44fa141514
Felipe Bergamo <fbbergamo@gmail.com>
2019-10-07 20:17:24 -0300

    statsd to instrument statsd

--- lib/sneakers/metrics/statsd_metrics.rb
+++ lib/sneakers/metrics/statsd_metrics.rb
@@ -14,8 +14,16 @@ module Sneakers
         block.call
         @connection.timing(metric, ((Time.now - start)*1000).floor)
       end
-
     end
   end
 end
 
+# `Sneakers` gem was originally implemented with `statsd-ruby` in mind and expects the
+# instance passed to StatsdMetrics to respond to a method called `timing`.
+# If you want to use the `statsd-instrument` gem instead of the `statsd-ruby`,
+# you need to ensure the `statsd-instrument` implements the `timing` method:
+module StatsD
+  def self.timing(metric, ms)
+    self.measure(metric, ms)
+  end
+end
